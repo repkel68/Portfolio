41b65d13f5247b7b63e78e29d088e486dd4fce7a
repkel68 <149861746+repkel68@users.noreply.github.com>
2025-04-14 20:21:38 +0900

    goHome

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import path from 'path';
+const __dirname = path.resolve();
+
+const app = express();
+const port = 80;
+
+app.use(express.static("frontend"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "\\frontend\\src\\html\\home.html");
+});
+
+app.listen(port, () => {
+    console.log('server is listening at localhost: '+ port);
+});
